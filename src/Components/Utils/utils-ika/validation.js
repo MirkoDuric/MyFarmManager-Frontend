b013768ee43e657a279ab3cb635d21715b9420d6
MirkoDuric <mirkodjuricczv@gmail.com>
2024-11-14 23:36:14 +0100

    validation.js file kompletiran

--- src/Components/Utils/utils-ika/validation.js
+++ src/Components/Utils/utils-ika/validation.js
@@ -1,6 +1,16 @@
-const isValidEmail = (email) => /\S+@\S+\.\S+/.test(email);
-const isNonEmptyString = (str) =>
-  typeof str === "string" && str.trim().length > 0;
-const isPositiveInteger = (num) => Number.isInteger(num) && num > 0;
+export const validateReminderText = (text) => {
+  return (
+    typeof text === "string" && text.trim().length > 0 && text.length <= 200
+  );
+};
 
-export { isValidEmail, isNonEmptyString, isPositiveInteger };
+export const validateReminderDate = (date) => {
+  const today = new Date();
+  const reminderDate = new Date(date);
+  return reminderDate > today;
+};
+
+export const isValidEmail = (email) => /\S+@\S+\.\S+/.test(email);
+export const isNonEmptyString = (str) =>
+  typeof str === "string" && str.trim().length > 0;
+export const isPositiveInteger = (num) => Number.isInteger(num) && num > 0;
