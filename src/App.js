924eee125c924c21fbd274a66fe738d2d9150c20
MirkoDuric <mirkodjuricczv@gmail.com>
2024-11-18 19:37:58 +0100

    Renamed components

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import ListaSvinja from "./Components/ListaSvinja/ListaSvinja";
-import Homepage from "./Components/Homepage/Homepage";
+import Landingpage from "./Components/Landingpage/Landingpage";
 import Login from "./Components/Login/Login";
 import Signup from "./Components/Signup/Signup";
-import Logedinpage from "./Components/Logedinpage/Logedinpage";
+import Homepage from "./Components/Homepage/Homepage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<Landingpage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/logedinpage" element={<Logedinpage />} />
+        <Route path="/homepage" element={<Homepage />} />
         <Route path="/listaSvinja" element={<ListaSvinja />} />
       </Routes>
     </>
